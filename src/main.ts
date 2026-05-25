b030bf177389562dc6c679147edd4d6b9a2aa1c8
Rob Curcio <guitarist4life92@gmail.com>
2026-05-25 08:09:46 -0400

    Updated example in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import * as myZ from "./my-zod";
 
 const myZodSchema = myZ.object({
-    name: myZ.optional(myZ.string().min(4)),
-    age: myZ.number().min(40).optional()
+    name: myZ.string(),
+    age: myZ.number().optional()
 });
 
-const val = "sdfsdf";
-
-type myZType = myZ.infer<typeof myZodSchema>;
+const val = {
+    name: "Rob",
+};
 
 const myZodResult = myZodSchema.safeParse(val);
 
